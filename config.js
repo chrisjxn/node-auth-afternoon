b22df6d080cd00a92bab4f2d8df8495624a87e91
chrisjxn <usjacksons@gmail.com>
2017-10-25 15:56:58 -0600

    commit changes

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    domain: 'chrisjxn.auth0.com',
+    clientID: 'xg-ySSa3pCtRDM6DyPxwbtV2UTpFyUHE',
+    clientSecret: 'HgeeNU8paZeZkLwQZH3SBPKtgxAFSFcjghrUT6Vse1uxUt-sRjBdPyenJ86rHPX1',
+};
